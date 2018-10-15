ffd474e45a8bf1b302304e2c76090468ca6d7fb7
luisfbgs <arueluis@gmail.com>
2018-10-15 10:06:59 -0300

    Function 'get_value' added to convert an operand into its value

--- assembler/src/helpers.cpp
+++ assembler/src/helpers.cpp
@@ -1,44 +1,82 @@
-#include <string>
 #include <sstream>
+#include <string>
 #include <vector>
 #include <iterator>
+#include <map>
 #include "helpers.hpp"
+#include "tables.hpp"
 
 namespace Helpers{
 
-template<typename Out>
-void split(const std::string &s, char delim, Out result) {
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        *(result++) = item;
-    }
-}
+	template<typename Out>
+		void split(const std::string &s, char delim, Out result) {
+			std::stringstream ss(s);
+			std::string item;
+			while (std::getline(ss, item, delim)) {
+				*(result++) = item;
+			}
+		}
 
-std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
+	std::vector<std::string> split(const std::string &s, char delim) {
+		std::vector<std::string> elems;
+		split(s, delim, std::back_inserter(elems));
+		return elems;
+	}
 
-std::vector<std::string> split_invisible_semicolon(const std::string &s) {
-    std::vector<std::string> elems;
-    std::string aux;
-    for (char c : s) {
-        if (isspace(c) && !aux.empty()) {
-            elems.push_back(aux);
-            aux = "";
-        }
-        else if (c == ';') {
-            break;
-        }
-        else if (!isspace(c)) {
-            aux += c;
-        }
-    }
-    if (!aux.empty()) elems.push_back(aux);
-    return elems;
-}
+	std::vector<std::string> split_invisible_semicolon(const std::string &s) {
+		std::vector<std::string> elems;
+		std::string aux;
+		for (char c : s) {
+			if (isspace(c) && !aux.empty()) {
+				elems.push_back(aux);
+				aux = "";
+			}
+			else if (c == ';') {
+				break;
+			}
+			else if (!isspace(c)) {
+				aux += c;
+			}
+		}
+		if (!aux.empty()) elems.push_back(aux);
+		return elems;
+	}
+
+	bool is_number(std::string &s) {
+		for(char c : s) {
+			if(!std::isdigit(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	uint8_t get_value(std::string operand) {
+		std::string aux;
+		int pos = 0;
+		uint8_t value = 0;
+		int sign = 1;
+
+		while (pos < operand.size()) {
+			while (pos < operand.size() && operand[pos] != '+' && operand[pos] != '-') {
+				aux += operand[pos++];
+			}
+
+			if(Helpers::is_number(aux)){
+				value += sign *  std::stoi(aux);
+			}
+			else{
+				value += sign * Tables::symbols.at(aux);
+			}
+
+			aux = "";
+			if(pos < operand.size()) {
+				sign = operand[pos++] == '+' ? 1 : -1;
+			}
+		}
+
+		return value;
+	}
 
 } // end namespace Helpers
 
