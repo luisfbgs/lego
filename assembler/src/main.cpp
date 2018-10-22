4aa255bcd7dba9065ea2d8bd218e7ff25848428e
luisfbgs <arueluis@gmail.com>
2018-10-22 18:09:23 -0300

    Main agora recebe o arquivo a ser montado como parâmetro

--- assembler/src/main.cpp
+++ assembler/src/main.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
 #include "twopass.hpp"
 
-int main() {
-	TwoPass algorithm("teste");
+int main(int argc, const char *argv[]) {
+	if(argc != 2) {
+		std::cout << "Deve ser fornecido o nome do arquivo a ser montado" << std::endl;
+		return 1;
+	}
+	TwoPass algorithm(argv[1]);
 	algorithm.second_pass(algorithm.first_pass());
 }
