36839064443c5d2973a9baaa6b97f2645a084427
luisfbgs <arueluis@gmail.com>
2018-11-26 12:09:09 -0200

    IO instructions separated in different procedures

--- translator/src/translator.cpp
+++ translator/src/translator.cpp
@@ -14,82 +14,103 @@ Translator::Translator (std::string source_file) {
 }
 
 void convert_add (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("add eax, ["+ operand + "]");
+    ia32.push_back("    add eax, ["+ operand + "]");
 }
 
 void convert_sub (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("sub eax, ["+ operand + "]");
+    ia32.push_back("    sub eax, ["+ operand + "]");
 }
 
 void convert_mult (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("mov edx, ["+ operand + "]");
-    ia32.push_back("imul edx");
+    ia32.push_back("    mov edx, ["+ operand + "]");
+    ia32.push_back("    imul edx");
 }
 
 void convert_div (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("cdq");
-    ia32.push_back("mov ecx, [" + operand + "]");
-    ia32.push_back("idiv ecx");
+    ia32.push_back("    cdq");
+    ia32.push_back("    mov ecx, [" + operand + "]");
+    ia32.push_back("    idiv ecx");
 }
 
 void convert_jmp (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("jmp ["+ operand + "]");
+    ia32.push_back("    jmp ["+ operand + "]");
 }
 
 void convert_jmpn (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("comp eax, 0");
-    ia32.push_back("jl " + operand);
+    ia32.push_back("    comp eax, 0");
+    ia32.push_back("    jl " + operand);
 }
 
 void convert_jmpp (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("comp eax, 0");
-    ia32.push_back("jg " + operand);
+    ia32.push_back("    comp eax, 0");
+    ia32.push_back("    jg " + operand);
 }
 
 void convert_jmpz (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("comp eax, 0");
-    ia32.push_back("je " + operand);
+    ia32.push_back("    comp eax, 0");
+    ia32.push_back("    je " + operand);
 }
 
 void convert_copy (std::string operand1, std::string operand2, std::vector<std::string> &ia32) {
-    ia32.push_back("mov ebx, [" + operand1 + "]");
-    ia32.push_back("mov [" + operand2 + "], ebx");
+    ia32.push_back("    mov ebx, [" + operand1 + "]");
+    ia32.push_back("    mov [" + operand2 + "], ebx");
 }
 
 void convert_load (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("mov eax, [" + operand + "]");
+    ia32.push_back("    mov eax, [" + operand + "]");
 }
 
 void convert_store (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("mov [" + operand + "], eax");
+    ia32.push_back("    mov [" + operand + "], eax");
 }
 
 void convert_section (std::string operand, std::vector<std::string> &ia32) {
     std::transform(operand.begin(), operand.end(), operand.begin(), ::tolower);
-    ia32.push_back("section ." + operand);
+    ia32.push_back("    section ." + operand);
 }
 
 void convert_space (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("resd " + operand);
+    ia32.push_back("    resd " + operand);
 }
 
 void convert_const (std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("dd " + operand);
+    ia32.push_back("    dd " + operand);
 }
 
 void convert_stop (std::vector<std::string> &ia32) {
-    ia32.push_back("mov eax, 1");
-    ia32.push_back("mov ebx, 0");
-    ia32.push_back("int 80h");
+    ia32.push_back("    mov eax, 1");
+    ia32.push_back("    mov ebx, 0");
+    ia32.push_back("    int 80h");
 }
 
-void convert_in_out (int type, int size, std::string operand, std::vector<std::string> &ia32) {
-    ia32.push_back("push eax");
-    ia32.push_back("push " + std::to_string(size));
-    ia32.push_back("push " + std::to_string(type + 2));
-    ia32.push_back("push " + operand);
-    ia32.push_back("call _in_out_c");
-    ia32.push_back("pop eax");
+void convert_in_c (std::string operand, std::vector<std::string> &ia32) {
+    ia32.push_back("    push eax");
+    ia32.push_back("    push " + operand);
+    ia32.push_back("    call LeerChar");
+    ia32.push_back("    pop eax");
+}
+
+void convert_out_c (std::string operand, std::vector<std::string> &ia32) {
+    ia32.push_back("    push eax");
+    ia32.push_back("    push " + operand);
+    ia32.push_back("    call EscreverChar");
+    ia32.push_back("    pop eax");
+}
+
+void convert_in_s (std::string operand1, std::string operand2, std::vector<std::string> &ia32) {
+    ia32.push_back("    push eax");
+    ia32.push_back("    push " + operand2);
+    ia32.push_back("    push " + operand1);
+    ia32.push_back("    call LeerString");
+    ia32.push_back("    pop eax");
+}
+
+void convert_out_s (std::string operand1, std::string operand2, std::vector<std::string> &ia32) {
+    ia32.push_back("    push eax");
+    ia32.push_back("    push " + operand2);
+    ia32.push_back("    push " + operand1);
+    ia32.push_back("    call EscreverString");
+    ia32.push_back("    pop eax");
 }
 
 std::vector<std::string> Translator::izi_to_ia32 (Line izi_line) {
@@ -148,16 +169,16 @@ std::vector<std::string> Translator::izi_to_ia32 (Line izi_line) {
         convert_stop(ia32);
     }
     else if (operation == "input_c") {
-        convert_in_out(1, 1, operands[0], ia32);
+        convert_in_c(operands[0], ia32);
     }
     else if (operation == "output_c") {
-        convert_in_out(2, 1, operands[0], ia32);
+        convert_out_c(operands[0], ia32);
     }
     else if (operation == "input_s") {
-        convert_in_out(1, stoi(operands[1]), operands[0], ia32);
+        convert_in_s(operands[0], operands[1], ia32);
     }
     else if (operation == "output_s") {
-        convert_in_out(2, stoi(operands[1]), operands[0], ia32);
+        convert_out_s(operands[0], operands[1], ia32);
     }
 
     if (!izi_line.label.empty()) {
@@ -184,13 +205,40 @@ std::vector<Line> Translator::pre_process () {
     return code;
 }
 
-void proc_in_out (std::vector<std::string> &ia32_code) {
-    ia32_code.push_back("_in_out_c: mov eax, [ESP+8]");
-    ia32_code.push_back("mov ebx, 1");
-    ia32_code.push_back("mov ecx, [ESP+4]");
-    ia32_code.push_back("mov edx, [ESP+12]");
-    ia32_code.push_back("int 80h");
-    ia32_code.push_back("ret"); 
+void proc_in_c (std::vector<std::string> &ia32_code) {
+    ia32_code.push_back("LeerChar:    mov eax, 3");
+    ia32_code.push_back("    mov ebx, 1");
+    ia32_code.push_back("    mov ecx, [ESP+4]");
+    ia32_code.push_back("    mov edx, 1");
+    ia32_code.push_back("    int 80h");
+    ia32_code.push_back("    ret 4"); 
+}
+
+void proc_out_c (std::vector<std::string> &ia32_code) {
+    ia32_code.push_back("EscreverChar:    mov eax, 4");
+    ia32_code.push_back("    mov ebx, 1");
+    ia32_code.push_back("    mov ecx, [ESP+4]");
+    ia32_code.push_back("    mov edx, 1");
+    ia32_code.push_back("    int 80h");
+    ia32_code.push_back("    ret 4"); 
+}
+
+void proc_in_s (std::vector<std::string> &ia32_code) {
+    ia32_code.push_back("LeerString:    mov eax, 3");
+    ia32_code.push_back("    mov ebx, 1");
+    ia32_code.push_back("    mov ecx, [ESP+4]");
+    ia32_code.push_back("    mov edx, [ESP+8]");
+    ia32_code.push_back("    int 80h");
+    ia32_code.push_back("    ret 8"); 
+}
+
+void proc_out_s (std::vector<std::string> &ia32_code) {
+    ia32_code.push_back("EscreverString:    mov eax, 4");
+    ia32_code.push_back("    mov ebx, 1");
+    ia32_code.push_back("    mov ecx, [ESP+4]");
+    ia32_code.push_back("    mov edx, [ESP+8]");
+    ia32_code.push_back("    int 80h");
+    ia32_code.push_back("    ret 8"); 
 }
 
 void Translator::translate (std::vector<Line> code) {
@@ -210,7 +258,10 @@ void Translator::translate (std::vector<Line> code) {
     }
 
     ia32_code.push_back("section .text");
-    proc_in_out(ia32_code);
+    proc_in_c(ia32_code);
+    proc_out_c(ia32_code);
+    proc_in_s(ia32_code);
+    proc_out_s(ia32_code);
 
     out.open(ia32_file);
 
